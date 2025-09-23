d91586e36a528352a0dd6fed4ed3f60e2f7d076d
Manu Sharma <sharmamanu2727@gmail.com>
2025-09-24 03:30:24 +0530

    adding new launch file, for the feature part, this commit only has the launch file - currenly there are many issues in the other files that are dependent on the launch file so please use the main branch, until this is updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/launch_ninja_turtle.py']),
+        ('share/' + package_name + '/launch', ['launch/launch_ninja_turtle.py', 'launch/launch_draw_with_me.py']),
     ],
     install_requires=[
         'setuptools',
@@ -30,6 +30,8 @@ setup(
             'voice_listener = ninja_turtle.voice_listener:main',
             'dummy_voice_listener = ninja_turtle.dummy_voice_listener:main',
             'circle = ninja_turtle.circle:main',
+            'game_manager = ninja_turtle.game_manager:main',
+            'tts_node = ninja_turtle.tts_node:main',
         ],
     },
 )
